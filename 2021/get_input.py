632feccdc24c389fb0219848906cb89e5f6deb43
apnamanya <anamanya@broadinstitute.org>
2021-12-02 15:46:46 -0500

    Automting getting my input and adding a readme.

---
+++ 2021/get_input.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""Retrieve input for a provided day in Advent of Code."""
+import argparse
+import os
+import sys
+
+import requests
+
+# Instuctions on retrieving your session key for use with this script:
+# https://github.com/wimglenn/advent-of-code-wim/issues/1
+
+def parse_arguments():
+    """Parse command-line arguments using argparse."""
+    parser = argparse.ArgumentParser(description="Input getter")
+    parser.add_argument("-d", "--day", help="The day to retrieve", required=True)
+
+    return parser.parse_args()
+
+
+def get_input(day):
+    """Retrieve the input for the AoC day."""
+    print(f"Day parsed is day{day}.")
+    url = f"https://adventofcode.com/2021/day/{day}/input"
+    pwd = os.path.realpath(os.path.dirname(__file__))
+
+    with open(os.path.join(pwd, "session.txt"), "r") as filep:
+        cookie = filep.readline().strip()
+
+    sess = requests.Session()
+    jar = requests.cookies.RequestsCookieJar()
+    jar.set("session", cookie)
+    res = sess.get(url, cookies=jar)
+    
+
+    with open(f"input_day{day}.txt", "w") as filep:
+        filep.write(res.text.strip())
+
+
+def main():
+    """Execute the script."""
+    args = parse_arguments()
+    print(f"Getting the input for day{args.day}")
+    
+
+    get_input(args.day)
+
+    return 0
+
+
+if __name__ == "__main__":
+    RET = main()
+
+    sys.exit(RET)

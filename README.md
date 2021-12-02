c84d03822d27b3690f219ecb1de4f09df83828a3
apnamanya <anamanya@broadinstitute.org>
2021-12-02 15:58:51 -0500

    Better code structuring.

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
-# advent_of_code
+## Advent of Code 2021.
+Writing all this so I don't forget, if it helps someone else great. Here are the steps
+1. Follow this [resource](https://github.com/wimglenn/advent-of-code-wim/issues/1) on how  to get your cookie session 
+2. Save the session key in a session.txt file. Make sure you do not commit this file to your github as it contains a secret. 
+3. Modify the code in get_input.py ([copied from](https://github.com/coreone/aoc-2021/blob/main/get.py) and modified) so you can get your input each day. 
+4. Run the script with the command (e.g get_input -d 2 -y 2021 to get day 2 input of even 2021)
+``` 
+get_input -d <day> -y 2021
+```
+5. Go code away and get the solutions submitted.
+**Note**: The day inputs are downloaded in the path from which you run the code as year/input_day{day}.txt (e.g 2021/nput_day2.txt). It creates the year dir if it does not already exist.
 
-Me writing quick solutions to the [advent of code](https://adventofcode.com/) challenges. It aint pretty but it works.
--- get_input.py
+++ get_input.py
@@ -14,26 +14,31 @@ def parse_arguments():
     """Parse command-line arguments using argparse."""
     parser = argparse.ArgumentParser(description="Input getter")
     parser.add_argument("-d", "--day", help="The day to retrieve", required=True)
+    parser.add_argument("-y", "--year", help="The year of event to retrieve", required=True)
 
     return parser.parse_args()
 
 
-def get_input(day):
+def get_input(day, year):
     """Retrieve the input for the AoC day."""
     print(f"Day parsed is day{day}.")
-    url = f"https://adventofcode.com/2021/day/{day}/input"
+    url = f"https://adventofcode.com/{year}/day/{day}/input"
     pwd = os.path.realpath(os.path.dirname(__file__))
 
     with open(os.path.join(pwd, "session.txt"), "r") as filep:
         cookie = filep.readline().strip()
 
+    yeardir = os.path.join(pwd, f"{year}")
+    if not os.path.isdir(yeardir):
+        os.mkdir(yeardir, 0o755)
+
     sess = requests.Session()
     jar = requests.cookies.RequestsCookieJar()
     jar.set("session", cookie)
     res = sess.get(url, cookies=jar)
-    
 
-    with open(f"input_day{day}.txt", "w") as filep:
+
+    with open(f"{year}/input_day{day}.txt", "w") as filep:
         filep.write(res.text.strip())
 
 
@@ -43,7 +48,7 @@ def main():
     print(f"Getting the input for day{args.day}")
     
 
-    get_input(args.day)
+    get_input(args.day, args.year)
 
     return 0
 
